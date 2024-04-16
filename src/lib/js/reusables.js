34049af14bb325ee836cc2716a0bbe1a2556b9df
Marc Aaron <mapasaraba@gmail.com>
2024-04-16 23:01:41 +0800

    fixed capitalizing letters replace - or _ with space

--- src/lib/js/reusables.js
+++ src/lib/js/reusables.js
@@ -1,15 +1,14 @@
 export function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  string = string.replace(/[-_]/g, " ");
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
-
 export function formatPokemonId(id) {
-    let idString = id.toString();
-    if (idString.length <= 3) {
-      let zerosNeeded = 3 - idString.length;
-      return "0".repeat(zerosNeeded) + idString;
-    } else {
-      return idString;
-    }
+  let idString = id.toString();
+  if (idString.length <= 3) {
+    let zerosNeeded = 3 - idString.length;
+    return "0".repeat(zerosNeeded) + idString;
+  } else {
+    return idString;
   }
+}
